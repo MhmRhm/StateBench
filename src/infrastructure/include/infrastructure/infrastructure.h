561499cf97f857e1b4ae55b1b3493167ba59eb17
Mohammad Rahimi <rahimi.mhmmd@gmail.com>
2023-07-01 09:06:51 +0800

    fix: resolve build issues

--- src/infrastructure/include/infrastructure/infrastructure.h
+++ src/infrastructure/include/infrastructure/infrastructure.h
@@ -1,11 +1,11 @@
 #pragma once
-#include <iostream>
-#include "boost/statechart/state_machine.hpp"
 #include "boost/statechart/simple_state.hpp"
+#include "boost/statechart/state_machine.hpp"
+#include <iostream>
 
 struct Greeting;
-struct Machine : boost::statechart::state_machine<Machine, Greeting>{};
+struct Machine : boost::statechart::state_machine<Machine, Greeting> {};
 struct Greeting : boost::statechart::simple_state<Greeting, Machine> {
-    Greeting() { std::cout << "Hello World!\n"; } // entry
-    ~Greeting() { std::cout << "Bye Bye World!\n"; } // exit
+  Greeting() { std::cout << "Hello World!\n"; }    // entry
+  ~Greeting() { std::cout << "Bye Bye World!\n"; } // exit
 };
--- src/infrastructure/infrastructure.cpp
+++ src/infrastructure/infrastructure.cpp
@@ -1,1 +1,12 @@
 #include "include/infrastructure/infrastructure.h"
+
+int main() {
+  Machine myMachine;
+  // The machine is not yet running after construction. We start
+  // it by calling initiate(). This triggers the construction of
+  // the initial state Greeting
+  myMachine.initiate();
+  // When we leave main(), myMachine is destructed what leads to
+  // the destruction of all currently active states.
+  return 0;
+}
